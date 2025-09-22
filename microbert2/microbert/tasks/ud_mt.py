9d0f0d294257416a41212c6ca72f11a77c2a377f
Phakphum Artkaew <pma5244@psu.edu>
2025-09-21 20:44:03 -0400

    projection

--- microbert2/microbert/tasks/ud_mt.py
+++ microbert2/microbert/tasks/ud_mt.py
@@ -27,6 +27,7 @@ class MTHead(torch.nn.Module, FromParams):
             mbert_model_name: str = "facebook/mbart-large-50-many-to-many-mmt",
             use_layer_mix: bool = True,
             freeze_decoder: bool = True,
+            project_to_decoder_dim: bool = True,
     ):
         super().__init__()
         self.use_layer_mix = use_layer_mix
@@ -35,6 +36,9 @@ class MTHead(torch.nn.Module, FromParams):
             self.mix = ScalarMix(num_layers)
 
         self.mbart = AutoModelForSeq2SeqLM.from_pretrained(mbert_model_name)
+        d_model = self.mbart.config.d_model  
+        if project_to_decoder_dim and embedding_dim != d_model:
+            self.proj = torch.nn.Linear(embedding_dim, d_model)
 
         if  freeze_decoder:
             for param in self.mbart.model.decoder.parameters():
