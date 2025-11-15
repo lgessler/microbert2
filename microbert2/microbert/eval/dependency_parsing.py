e411710ca699f040afd8f8f67e4b726fc2c291d2
Phakphum Artkaew <pma5244@psu.edu>
2025-11-14 23:07:43 -0500

    diaparser

--- microbert2/microbert/eval/dependency_parsing.py
+++ microbert2/microbert/eval/dependency_parsing.py
@@ -218,7 +218,10 @@ class EvaluateDependencyParsing(Step):
         self.logger.info(f"Test data: {test_data_path}")
 
 
-        import diaparser
+        import diaparser.utils.config
+        import diaparser.utils.field
+        import diaparser.parsers
+        import diaparser.parsers.biaffine_dependency
 
         torch.serialization.add_safe_globals([
             diaparser.utils.config.Config,
